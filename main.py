fa43ccfbe6cdb7ab75ec3ad5feaaaa0bb02a2172
Praveenk-nani <5747praveen@gmail.com>
2024-09-23 13:22:41 +0530

    adding more files

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 from fastapi.middleware.cors import CORSMiddleware
+import cv2
+import logging
 
 
 app = FastAPI()
+
+
+
+logging.basicConfig(level=logging.INFO)
+
 
 
 origins = [
@@ -35,30 +42,39 @@ async def hello():
     return {"hello":"Praveenkumar"}
 
 
-def read_image(data)->np.ndarray:
+def read_image(data)->list:
     try:
         image = np.array(Image.open(BytesIO(data)))
-        return image
+        new_img = cv2.resize(image,(224,224))
+
+        return [new_img,False]
     except Exception as e:
-        raise ValueError(f"while processing image : {e}")
+        return [e,True]
 
 
 @app.post("/predictions")
 async def predict(file:UploadFile=File(...)):
     try:
-        # bytes = await file.read()
-        # img = read_image(bytes)
-        # img_batch = np.expand_dims(img,0)
-        # result_list = []
+        bytes = await file.read()
+        img = read_image(bytes)
+        logging.info(f"image file was received {file.filename}")
+        if img[1]:
+            logging.info(f"error has been occurred while converting the image")
+            return {"Result":f"Error {img[0]}",
+                    "Accuracy":"image not supported"}
+        else:
+            img_batch = np.expand_dims(img[0],0)
+            result_list = []
 
-        # prediction = MODEL.predict(img_batch)
-        # confidence = np.argmax(prediction)
-        # result = CLASSES[confidence]
-        # result_list = [result,str(prediction[0][confidence])]
-        # return {"Result":result_list[0],
-        #         "Accuracy":result_list[1]}
-        return {"Result":"successfully opened",
-                "Accuracy":file.filename}
+            prediction = MODEL.predict(img_batch)
+            confidence = np.argmax(prediction)
+            result = CLASSES[confidence]
+            result_list = [result,str(prediction[0][confidence])]
+            logging.info(f"prediction was successful")
+            return {"Result":result_list[0],
+                    "Accuracy":result_list[1]}
+        # return {"Result":"successfully opened",
+        #         "Accuracy":file.filename}
     except Exception as e:
         return {"Result":"Error",
                 "Accuracy":e}
